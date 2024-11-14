9b14a3aaff4e0854aff3429d68e7c5d6b14ba6fa
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2024-11-14 15:15:53 -0500

    wip
    wip

--- lib/domain/entities/objects/plant.dart
+++ lib/domain/entities/objects/plant.dart
@@ -4,8 +4,9 @@ import 'package:bonfire/bonfire.dart';
 import 'package:my_cool_game/domain/core/globals.dart';
 
 class Plant extends GameDecoration {
-  Plant({required super.position})
-      : super.withSprite(
+  Plant({
+    required super.position,
+  }) : super.withSprite(
           sprite: Sprite.load('plant/${Random().nextInt(18)}.png'),
           size: Vector2.all(Globals.tileSize),
         );

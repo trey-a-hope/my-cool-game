28877d8b6396da3de6f2f7556ea0ee2a80802b36
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2024-12-03 15:56:03 -0500

    wip
    wip

--- lib/domain/core/globals.dart
+++ lib/domain/core/globals.dart
@@ -20,6 +20,27 @@ class Globals {
   static const tileSize = 32.0;
 }
 
+class _Forces {
+  final gravity = AccelerationForce2D(
+    id: 'gravity',
+    value: Vector2(0, 400),
+  );
+}
+
+class _Input {
+  static const path = 'input/';
+
+  final aPressed = '${path}a_pressed.png';
+  final aUnpressed = '${path}a_unpressed.png';
+  final bPressed = '${path}b_pressed.png';
+  final bUnpressed = '${path}b_unpressed.png';
+  final xPressed = '${path}x_pressed.png';
+  final xUnpressed = '${path}x_unpressed.png';
+  final yPressed = '${path}y_pressed.png';
+  final yUnpressed = '${path}y_unpressed.png';
+  final leftJoystick = '${path}left_joystick.png';
+}
+
 class _Map {
   final name = 'map.json';
 
@@ -52,25 +73,4 @@ class _Map {
           position: properties,
         ),
   };
-}
-
-class _Input {
-  static const path = 'input/';
-
-  final aPressed = '${path}a_pressed.png';
-  final aUnpressed = '${path}a_unpressed.png';
-  final bPressed = '${path}b_pressed.png';
-  final bUnpressed = '${path}b_unpressed.png';
-  final xPressed = '${path}x_pressed.png';
-  final xUnpressed = '${path}x_unpressed.png';
-  final yPressed = '${path}y_pressed.png';
-  final yUnpressed = '${path}y_unpressed.png';
-  final leftJoystick = '${path}left_joystick.png';
-}
-
-class _Forces {
-  final gravity = AccelerationForce2D(
-    id: 'gravity',
-    value: Vector2(0, 400),
-  );
 }

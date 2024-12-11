e76e717caa894df04767f8455673ea81a2b47c7f
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2024-12-11 11:32:56 -0500

    wip
    wip

--- test/integration_test.dart
+++ test/integration_test.dart
@@ -4,8 +4,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:my_cool_game/domain/core/providers.dart';
+import 'package:my_cool_game/domain/entities/players/dwarf_warrior.dart';
 import 'package:my_cool_game/domain/notifiers/game_progress_notifier.dart';
 import 'package:my_cool_game/presentation/my_cool_game.dart';
+import 'package:my_cool_game/presentation/overlays/game_over_overlay.dart';
 
 class MockWidgetRef extends Mock implements WidgetRef {}
 
@@ -14,11 +16,26 @@ class MockBonfireGame extends Mock implements BonfireGame {}
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
+  late DwarfWarrior warrior;
+
   late Widget gameWidget;
+  late MockWidgetRef ref;
+  late MockBonfireGame mockGame;
 
   const averageAnimationDuration = Duration(seconds: 1);
 
   setUp(() {
+    ref = MockWidgetRef();
+
+    mockGame = MockBonfireGame();
+
+    warrior = DwarfWarrior(
+      ref,
+      position: Vector2.all(20),
+    );
+
+    warrior.gameRef = mockGame;
+
     gameWidget = ProviderScope(
       // Override providers at the root
       overrides: [
@@ -40,18 +57,32 @@ void main() {
     'Game Overlay',
     () {
       testWidgets(
-        'StartOverlay renders by default.',
+        'Shows game over overlay when player dies',
         (WidgetTester tester) async {
           await tester.pumpWidget(gameWidget);
           await tester.pump(const Duration(seconds: 1));
 
           final bonfireWidget =
               tester.widget<BonfireWidget>(find.byType(BonfireWidget));
+          final player = bonfireWidget.player as DwarfWarrior;
+
+          debugPrint('Initial player life: ${player.life}');
+
+          player.onReceiveDamage(AttackOriginEnum.ENEMY, 100, null);
+          debugPrint('After damage player life: ${player.life}');
+          debugPrint('Is player dead: ${player.isDead}');
 
           await tester.pump(averageAnimationDuration);
 
-          // TODO: Coming in future branch...
-          // expect(find.byType(StartOverlay), findsOneWidget);
+          expect(find.byType(GameOverOverlay), findsOneWidget);
+
+          final resetButton = find.text('Try Again?');
+          expect(resetButton, findsOneWidget);
+
+          await tester.tap(resetButton);
+          await tester.pump(const Duration(milliseconds: 500));
+
+          expect(find.byType(GameOverOverlay), findsNothing);
         },
       );
     },

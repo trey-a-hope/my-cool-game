f3af5d5b1b0d417e5223878e70bb7a9429ed9c6c
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2024-12-15 01:09:50 -0500

    wip
    wip

--- lib/domain/core/globals.dart
+++ lib/domain/core/globals.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class Globals {
   Globals._();
 
+  static final audio = _Audio();
   static final forces = _Forces();
   static final imageAssets = _ImageAssets();
   static final input = _Input();
@@ -11,6 +12,29 @@ class Globals {
 
   static const spriteStepTime = 0.1;
   static const tileSize = 32.0;
+}
+
+class _Audio {
+  final backgroundMusic = 'background_music.mp3';
+  final chestOpening = 'chest_opening.wav';
+  final collectItem = 'collect_item.wav';
+  final dwarfWarriorAttack = 'dwarf_warrior_attack.mp3';
+  final dwarfWarriorDie = 'dwarf_warrior_die.wav';
+  final dwarfWarriorHurt = 'dwarf_warrior_hurt.wav';
+  final flame = 'flame.wav';
+  final gem = 'gem.flac';
+  final gameOver = 'game_over.wav';
+  final gameWon = 'game_won.wav';
+  final headlessHorsemanAttack = 'headless_horseman_attack.wav';
+  final headlessHorsemanDie = 'headless_horseman_die.wav';
+  final headlessHorsemanHurt = 'headless_horseman_hurt.wav';
+  final lizardManAttack = 'lizard_man_attack.wav';
+  final lizardManDie = 'lizard_man_die.wav';
+  final lizardManHurt = 'lizard_man_hurt.mp3';
+  final minotaurAttack = 'minotaur_attack.wav';
+  final minotaurDie = 'minotaur_die.wav';
+  final minotaurHurt = 'minotaur_hurt.wav';
+  final potion = 'potion.wav';
 }
 
 class _Forces {
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_cool_game/domain/core/globals.dart';
 import 'package:my_cool_game/presentation/my_cool_game.dart';
 import 'package:toastification/toastification.dart';
+
+final audio = [
+  Globals.audio.backgroundMusic,
+  Globals.audio.chestOpening,
+  Globals.audio.collectItem,
+  Globals.audio.dwarfWarriorAttack,
+  Globals.audio.dwarfWarriorDie,
+  Globals.audio.dwarfWarriorHurt,
+  Globals.audio.flame,
+  Globals.audio.gem,
+  Globals.audio.gameOver,
+  Globals.audio.gameWon,
+  Globals.audio.headlessHorsemanAttack,
+  Globals.audio.headlessHorsemanDie,
+  Globals.audio.headlessHorsemanHurt,
+  Globals.audio.lizardManAttack,
+  Globals.audio.lizardManDie,
+  Globals.audio.lizardManHurt,
+  Globals.audio.minotaurAttack,
+  Globals.audio.minotaurDie,
+  Globals.audio.minotaurHurt,
+  Globals.audio.potion,
+];
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
